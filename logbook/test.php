c1fac4feaf00b3d982368d53f73d2528babac598
Ekawa <55327310+EkawaD@users.noreply.github.com>
2020-04-06 15:04:09 +0200

    Update test.php

--- logbook/test.php
+++ logbook/test.php
@@ -1,16 +1,11 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Notre première instruction : echo</title>
         <meta charset="utf-8" />
+        <title>Ma page web</title>
     </head>
     <body>
-        <h2>Affichage de texte avec PHP</h2>
-        
-        <p>
-            Cette ligne a été écrite entièrement en HTML.<br />
-            <?php 
-            echo "Celle-ci a été écrite entièrement en PHP."; ?>
-        </p>
+        <h1>Ma page web</h1>
+        <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
     </body>
 </html>
