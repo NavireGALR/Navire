e280527368439bd42c48435dc2b60b696973b58f
sebachien <sebastien.louis.69@gmail.com>
2020-05-15 13:37:05 +0200

    Update manager.php

--- logbook_mvc/model/manager.php
+++ logbook_mvc/model/manager.php
@@ -5,7 +5,7 @@ class Manager
 
 	protected function dbConnect() 
     {
-        $db = new PDO('mysql:host=localhost;dbname=logbook;port=3308;charset=utf8', 'root', '');
+        $db = new PDO('mysql:host=localhost;dbname=sebastien;port=3308;charset=utf8', 'root', '');
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $db;
     }
