12549be82729a36fff1fd880b1fb41262d6166bb
Ekawa <steo.ederhy@gmail.com>
2020-05-20 14:39:53 +0200

    added attr instead of caract to avoid confusion

--- GAMEBOOK/caract.py
+++ GAMEBOOK/caract.py
@@ -3,7 +3,11 @@ from parser import Parser
 class Caract(object):
 	"""docstring for Caract"""
 	def __init__(self, type):
+		attr = Parser.caract_from_json('attr.json')
+	
 		self.ressources = Parser.caract_from_json('ressources.json')
-		self.attr = Parser.caract_from_json('attr.json')
+		self.attr_perso = attr[0]
+		self.attr_ressources = attr[1]
+		
 
 		
--- GAMEBOOK/personnage.py
+++ GAMEBOOK/personnage.py
@@ -13,7 +13,7 @@ class Personnage(object):
 
 	def __init__(self):
 		caract = Caract()
-		self.attr = caract.attr
+		self.attr = caract.attr[0]
 		self.sac = Sac()
 
 
@@ -35,6 +35,19 @@ class Joueur(Personnage):
 	def up(self)
 		self.attr['level'] += 1
 		self.attr['xp'] = 0
+
+
+class Monstres(Personnage):
+	"""Décrit les caractéristiques et actions d'un personnage
+	non-joueur
+
+	init : 
+
+	"""
+
+	def __init__(self):
+		super().__init__()
+
 
 		
 
