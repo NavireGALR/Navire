7c1ecd34e9d89892267cd87582ccd5d99533adf9
Ekawa <steo.ederhy@gmail.com>
2020-05-20 14:15:12 +0200

    modified since caract class

--- GAMEBOOK/personnage.py
+++ GAMEBOOK/personnage.py
@@ -1,19 +1,41 @@
 from parser import Parser
+from caract import Caract
 
 class Personnage(object):
 	"""Décrit les caractéristiques et actions d'un personnage
 	joueur ou non-joueur
 
-	init : 
+	init : attributs level et sac
 
 	"""
 
 	DIE = 1
 
 	def __init__(self):
-		self.caract = Parser.caract_from_json('caract.json')
-		self.level = 0
+		caract = Caract()
+		self.attr = caract.attr
 		self.sac = Sac()
+
+
+
+class Joueur(Personnage):
+	"""Décrit les caractéristiques et actions d'un personnage
+	joueur 
+
+	init : 
+
+	"""
+
+	def __init__(self):
+		super().__init__()
+
+	def get_xp(self, xp_gagne)
+		self.attr['xp'] += xp_gagne
+	
+	def up(self)
+		self.attr['level'] += 1
+		self.attr['xp'] = 0
+
 		
 
 
