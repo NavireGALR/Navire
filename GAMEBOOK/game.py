13520a945ec7fbc3120d6938ec92386b96eae564
Ekawa <steo.ederhy@gmail.com>
2020-05-20 13:04:31 +0200

    valid
    	nouveau fichier : GAMEBOOK/caract.json
    	nouveau fichier : GAMEBOOK/game.py
    	nouveau fichier : GAMEBOOK/gamebookUML.txt
    	nouveau fichier : GAMEBOOK/parser.py
    	nouveau fichier : GAMEBOOK/personnage.py

---
+++ GAMEBOOK/game.py
@@ -0,0 +1,8 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+ingame = int(input())
+
+while ingame:
+	fenetre = pygame.display.set_mode((640, 480))
---
+++ GAMEBOOK/personnage.py
@@ -0,0 +1,21 @@
+from parser import Parser
+
+class Personnage(object):
+	"""Décrit les caractéristiques et actions d'un personnage
+	joueur ou non-joueur
+
+	init : 
+
+	"""
+
+	DIE = 1
+
+	def __init__(self):
+		self.caract = Parser.caract_from_json('caract.json')
+		self.level = 0
+		self.sac = Sac()
+
+
+
+		
+		
