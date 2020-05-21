4e8c073f2239e38710a28ce4128b3c3a75f9af42
sebachien <sebastien.louis.69@gmail.com>
2020-05-21 19:30:17 +0200

    sert

---
+++ sebtest/const.py
@@ -0,0 +1,12 @@
+"""Constantes du jeu du tes"""
+
+#Paramètres de la fenêtre
+nombre_sprite_cote = 15
+taille_sprite = 30
+cote_fenetre = nombre_sprite_cote * taille_sprite
+
+#Personnalisation de la fenêtre
+titre_fenetre = "Jeu Test"
+
+#Listes des images du jeu
+
