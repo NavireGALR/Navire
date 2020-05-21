a25a5469fa2b868d1489bb08ffdcdbbf5572baec
Ekawa <steo.ederhy@gmail.com>
2020-05-21 20:34:15 +0200

    update terrain

--- GAMEBOOK/models/terrain.py
+++ GAMEBOOK/models/terrain.py
@@ -1,46 +1,56 @@
 import pygame
 from pygame.locals import * 
-from constantes import *
 
-
-
-class Foret:
-	"""Classe permettant de créer un niveau"""
-	def __init__(self, fichier):
-		self.fichier = fichier
-		self.structure = 0
+class Terrain(object):
+	"""docstring for Terrain"""
 	
-	
+	NB_SPRITE = 15
+	TAILLE_SPRITE = 30
+
+	def __init__(self, map):
+		self.debut = pygame.image.load("img/debut.png").convert()
+		self.mort = pygame.image.load("img/mort.png").convert_alpha()
+		self.map = map
+		self.structure = []
+
 	def generer(self):
-		with open(self.fichier, "r") as fichier:
+		with open(self.map, "r") as f:
 			structure_niveau = []
-			for ligne in fichier:
+			for ligne in f:
 				ligne_niveau = []
 				for sprite in ligne:
 					if sprite != '\n':
 						ligne_niveau.append(sprite)
 				structure_niveau.append(ligne_niveau)
 			self.structure = structure_niveau
-	
-	
+
 	def afficher(self, fenetre):
-		bois = pygame.image.load("img/bois.png").convert_alpha()
-		debut = pygame.image.load("img/debut.png").convert()
-		mort = pygame.image.load("img/mort.png").convert_alpha()
 		num_ligne = 0
 		for ligne in self.structure:
-			#On parcourt les listes de lignes
 			num_case = 0
 			for sprite in ligne:
-				#On calcule la position réelle en pixels
-				x = num_case * taille_sprite
-				y = num_ligne * taille_sprite
+				x = num_case * self.TAILLE_SPRITE
+				y = num_ligne * self.TAILLE_SPRITE
 				if sprite == 'b':		   #b = Bois
-					fenetre.blit(bois, (x,y))
+					fenetre.blit(self.obstacle, (x,y))
 				elif sprite == 'd':		   #d = Départ
-					fenetre.blit(debut, (x,y))
+					fenetre.blit(self.debut, (x,y))
 				elif sprite == 'm':		   #m = Mort
-					fenetre.blit(mort, (x,y))
+					fenetre.blit(self.mort, (x,y))
 				num_case += 1
 			num_ligne += 1
+		
+
+class Foret(Terrain):
+	"""Classe permettant de créer un niveau de type forêt"""
+	
+	def __init__(self, map):
+		super().__init__(map)
+		self.obstacle = pygame.image.load("img/bois.png").convert_alpha()
+		
+	
+	def afficher(self, fenetre):
+		super().afficher(fenetre)
+		
+		
 			
